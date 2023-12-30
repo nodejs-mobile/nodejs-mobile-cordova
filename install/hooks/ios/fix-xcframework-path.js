02a29fd2b3f7fcb95575f9cd6fed1ca1faa93395
Liming Xie <liming.xie@gmail.com>
2023-12-31 01:02:53 +0800

    ios: fix to support newer cordova-ios@7.0.1, use tar instead of tar.gz2

--- install/hooks/ios/fix-xcframework-path.js
+++ install/hooks/ios/fix-xcframework-path.js
@@ -5,7 +5,7 @@ module.exports = function(context) {
   // Require the iOS platform Api to get the Xcode .pbxproj path.
   var iosPlatformPath = path.join(context.opts.projectRoot, 'platforms', 'ios');
   var iosAPI = require(path.join(iosPlatformPath, 'cordova', 'Api'));
-  var iosAPIInstance = new iosAPI();
+  var iosAPIInstance = new iosAPI('ios', iosPlatformPath);
   var pbxprojPath = iosAPIInstance.locations.pbxproj;
   var rootIosProjDir = iosAPIInstance.locations.root;
   var cordovaProjPath = iosAPIInstance.locations.xcodeCordovaProj;
